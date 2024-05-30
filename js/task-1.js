809e8c9f8ab31e12528e0ebe98072331ae3472b2
Tina-Sl <slobodenukvm@gmail.com>
2024-05-30 10:40:34 +0200

    unnecessary code removed (task 1)

--- js/task-1.js
+++ js/task-1.js
@@ -1,18 +1,4 @@
-// 1. Зробимо зміни в HTML, поставимо все, що в body, в контейнер div (так краще буде виглядати)
-
-const elemP = document.querySelector("p");
-elemP.insertAdjacentHTML("beforebegin", '<div class="one-container"></div>');
-const container = document.querySelector(".one-container");
-const newElem = elemP.cloneNode(true);
-container.prepend(newElem);
-elemP.remove();
-
-const elemUl = document.querySelector("#categories");
-const categories = elemUl.cloneNode(true);
-container.append(categories);
-elemUl.remove();
-
-// 2. Даємо класи для всіх елементів ul, li, h2
+// 1. Даємо класи для всіх елементів ul, li, h2
 const lists = document.getElementsByTagName("ul");
 for (let i = 0; i < lists.length; i++) {
   if (lists[i].id !== "categories") {
@@ -29,7 +15,7 @@ for (const title of titles) {
   title.classList.add("category");
 }
 
-// 3. Виконання завдання: підрахунки і виведення на консоль
+// 2. Виконання завдання: підрахунки і виведення на консоль
 const items = document.querySelectorAll(".item");
 
 console.log(`Number of categories: ${items.length}`);
